fe923d66fa41551da2de3fbc9fcae5cb50c7ef52
yagiz <yagiz@MSI>
2024-10-09 20:13:23 +0300

    Add project files.

---
+++ BLL/DAL/Students.cs
@@ -0,0 +1,24 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DAL
+{
+    public class Students
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public double GPA { get; set; }
+
+
+    }
+}
